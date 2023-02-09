bdf1d64b79916746e272e30a04ce0382d7b8dc1e
AgoraGO-b2b <agoragoinfo@gmail.com>
2023-02-09 13:19:57 +0530

    Ai implemented

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ const gameBoard = (()=>{
         setsign
     }
 })();
-const player = (sign,turn)=>{
+const player = (sign,turn,ishuman)=>{
     
-    return{sign,turn}
+    return{sign,turn,ishuman}
 
 }
 const gameloop = (()=>{
@@ -25,7 +25,7 @@ const getWinner = ()=>{
  const checkforwin =(board)=>{
         console.log("hi")
         if(checkrow(board)||checkdiag(board)||checkcol(board)){
-            return true
+            endgame(getWinner())
         }
         
  }
@@ -91,36 +91,75 @@ const render = (()=>{
             console.log(field)
         return false}
     }
-    const iniitialize = (()=>{
-        const player1 = player("X",true)
-        const player2 = player("O",true)        
+    const setplayertwo = ()=>{
+        const ele = document.querySelectorAll("input[type='radio']")
+        return new Promise((resolve, reject) => {
+            ele.forEach(element => {
+                element.addEventListener("click",()=>{
+                   
+                        if(element.value == "human"){
+                            resolve(true)
+                        }
+                        else{
+                            resolve(false)
+                        }
+                    
+                })            
+            });
+        })
+        
+    }
+    const iniitialize = (async()=>{
+        const secondplayer = await setplayertwo();
+        const player1 = player("X",true,true)
+        const player2 = player("O",false,secondplayer)        
         var board = document.querySelectorAll(".item")
         for(let i=0;i<board.length;i++){
             board[i].addEventListener("click",()=>{
-                if(fieldNotOccupied(gameBoard.board[i])){
-                board[i].innerHTML = playerController.getturn(player1,player2)
-                gameBoard.setsign(i,board[i].innerHTML)
-                if(gameloop.checkforwin(gameBoard.board)){
-                        gameloop.endgame(gameloop.getWinner())
-                }
-                console.log(gameBoard.board)}
+                if(fieldNotOccupied(gameBoard.board[i]))
+                    {
+                        playerController.playturn(player1,player2,board,i)
+                        gameloop.checkforwin(gameBoard.board)      
+                    }
             })
         }
     })()
+    return {fieldNotOccupied}
 })()
 const playerController =(()=>{
-    const getturn = (player1,player2)=>{
+    const getfield = (board)=>{
+        var empty =[]
+        for(let i=0;i<board.length;i++){
+            if(render.fieldNotOccupied(board[i])){
+                empty.push(i)
+            }
+        }
+        return empty[Math.floor(Math.random()*empty.length)];
+    }
+    const playturn = (player1,player2,htmlboard,index)=>{
+        
         if(player1.turn == true){
             player1.turn = false
             player2.turn = true
-            return player1.sign
+            htmlboard[index].innerHTML = player1.sign
+            gameBoard.setsign(index,htmlboard[index].innerHTML)
+            if(player2.turn == true && player2.ishuman == false){
+                player1.turn = true
+                player2.turn = false
+                var AImove = getfield(gameBoard.board)
+                htmlboard[AImove].innerHTML = player2.sign
+                gameBoard.setsign(AImove,player2.sign)
+            }
         }
-        if(player2.turn == true){
+        //for two players
+        else if(player2.turn == true && player2.ishuman == true){
             player2.sign
             player1.turn = true
             player2.turn = false
-            return player2.sign
+            htmlboard[index].innerHTML = player2.sign
+            gameBoard.setsign(index,player2.sign)
         }
+        
     }
-    return{getturn}
+    return{playturn}
 })();
